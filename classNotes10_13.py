25d40d451c8a3ccc95e55d7ab82ee22836a118e4
Zack Considine <zack_considine@m015354.local>
2020-10-13 11:53:18 -0700

    added class notes from october 13th

---
+++ classNotes10_13.py
@@ -0,0 +1,78 @@
+# comments: comments always start with "#"
+# everything that follows a "#" is a comment and the computer
+# does not read it as code.
+''' Longer comments that span multiple lines
+
+
+you can use a triple quotation
+
+like this. Anything between triple quoatations is a comment.
+
+'''
+
+#Variables
+
+elephant = 10
+
+# called variable becuase we can change them.
+#x = x + 4
+
+pi = "done"
+#for printing, we do not need quotation marks around variables
+print(elephant)
+print(pi)
+
+# when printing variables, we print their VALUE
+
+# Compounds example
+
+myNum = 20
+myAn = "Chameleon"
+
+#print(myNum + myAn) does not work
+
+myNum = str(myNum)
+
+print(myNum + myAn)
+print(myNum + " " + myAn + "s")
+
+# when we add strings in python, we 'concatenate' them (push them together)
+
+# It works differently for int type variables. Use math operators.
+
+x = 6
+y = 2
+
+#we can see the data type with the function "type()"
+
+
+print(type(x + y)) #if you add two integers you always get an integer
+print(type(x*y)) #if you multiply two integers you always get an integer
+print(type(x/y)) #if you divide two integers you DO NOT always get an int
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
