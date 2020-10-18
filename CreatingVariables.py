d5a09bb9ce961ba42fbcce181944294d200f4307
Zack Considine <zack_considine@m015354.local>
2020-10-18 15:28:45 -0700

    added class notes

---
+++ CreatingVariables.py
@@ -0,0 +1,57 @@
+#Comment your name and date below.
+
+
+
+# 1) Create a variable named myBool and set it equal to True.
+
+
+
+
+if myBool:
+    print("You did it!")
+
+else:
+    print("try again")
+
+# 2) Create two variables that represent your favorite toppings on pizza.
+#     Name the variables top1 and top2
+
+
+
+
+print("I prefer my pizza with " + top1 + " and " + top2)
+
+# 3) Create a list of numbers with the variable name randL. Print out the list.
+#   Add a comment which describes what the output of the list looks like.
+
+
+
+
+
+
+
+# 4) Create three variables that all represent different data types.
+#    Name the variables var1, var2, and var3
+
+
+
+
+
+print(type(var1))
+print(type(var2))
+print(type(var3))
+
+# 5) Add a comment explaining why the previous code section did not print out
+#    the variable values.
+
+
+
+
+# 6) Create an integer variable and set it equal to 5.8. Print it out.
+#   Then cast the same variable as a float and print it out.
+#   Then cast the same variable as a string and print it out.
+
+
+
+
+
