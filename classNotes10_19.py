3f744710f1ad4986500f1ed03a61599be328797b
Zack Considine <zack_considine@m015354.local>
2020-10-19 12:51:41 -0700

    added class notes

---
+++ classNotes10_19.py
@@ -0,0 +1,49 @@
+#Class Notes 10/19
+# Mr. Considine
+
+# User input! Really important if you want your program to do anything
+# interactive or that solves an issue or question.
+
+# input has space for a user prompt.
+# the output (or what it is = to) is your variable name for the input
+
+'''data = input("Give me a number!")
+data = int(data)
+
+result = 5 * data
+
+print(result)'''
+
+#inputs are always given the string data type.
+
+#Loops! Loops in python are very similar to the functionality
+# of loops (repeats) in scratch
+
+#Definite loops are loops that repeat a certain number of times.
+
+for i in range(10):
+    print(i)
+
+# "i" is an iterator. It keeps track of which repetition it is currently on
+# iterators (and many other things) start with 0.
+
+#iterators can help you move through a list.
+# ["cat", "dog", "parrot", "fish", "unicorn"]
+# [position 0, position 1, position 2, position 3, position 4] (indexes)
+
+pets = ["cat", "dog", "parrot", "fish", "unicorn", "chameleon"]
+
+for i in range(len(pets)):
+    print(pets[i])
+
+# len() is a function you can use for a list to see how many elements are in the list
+
+
+
+
+
+
+
+
+
+    
