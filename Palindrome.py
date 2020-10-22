cecc7e09fe9e84e4ba0cf3c0a647dfffbeaeaab4
Zack Considine <zack_considine@m015354.local>
2020-10-21 20:55:35 -0700

    added notes on lists from 10/21/20

---
+++ Palindrome.py
@@ -0,0 +1,17 @@
+#Python program outline
+
+def main():
+
+    # every bit of code "within" main is the code that is run
+    # when main is run
+    # your main method should work as the control to do anything else
+    # in your program
+    print("hi")
+    
+
+    
+
+
+if __name__ == "__main__":
+    main()
+    
---
+++ samples.py
@@ -0,0 +1,65 @@
+# functions are colored as purple
+
+print("hello")
+
+x = str(5)
+
+'''print("hi " + 3)''' #this errors because python cannot combine str and int
+# data types
+
+print("hi " + x)
+
+# lists are defined by brackets []
+
+mariaList = ["raindrops", "whiskers", "kittens", "blue satin saches"]
+print(mariaList)
+
+# lists are great because they can hold multiple data values
+# we can add and remove with relative ease
+
+mariaList.append("packages in string")
+
+print(mariaList)
+
+mariaList.remove("raindrops")
+print(mariaList)
+
+# you can single-out elements in a list. you can look at a specific
+# element by its position (we count by starting with 0...)
+
+print(mariaList[2])
+
+# we can split strings into a list
+
+myMovie = "Sound of Music"
+myList = list(myMovie)
+print(myList)
+
+# variables as a concept: they are a placeholder name. The name doesn't
+# matter much. One way to think of it is like a label.
+
+highScore = 890
+
+# variables allow you to use the data without re-typing the data
+# in general we write variables with camel-case
+# variables must be only 1 word and cannot start with anything but
+# letters.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
