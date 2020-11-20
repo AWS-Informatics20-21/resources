5c8ef81dc3f5a739cd5ae8b5b783228562d4f13a
Zack Considine <zack_considine@m015354.local>
2020-11-19 20:21:20 -0800

    added Button

--- Button.py
+++ Button.py
@@ -4,16 +4,33 @@ class Button():
 
     def __init__(self, win, color, text, center, size):
 
-        p1 = Point(center.x - size, center.y - size/2)
-        p2 = Point(center.x + size, center.y + size/2)
-        self.r = Rectangle(p1, p2)
+        self.p1 = Point(center.x - size, center.y - size/2)
+        self.p2 = Point(center.x + size, center.y + size/2)
+        self.r = Rectangle(self.p1, self.p2)
         self.r.draw(win)
         self.r.setFill(color)
         self.t = Text(center, text)
         self.t.setSize(18)
         self.t.draw(win)
 
-
     def changeColor(self, newColor):
         self.r.setFill(newColor)
-        
+
+    def isClicked(self, p):
+        minX = self.p1.x
+        maxX = self.p2.x
+        minY = self.p1.y
+        maxY = self.p2.y
+
+        if p.x > minX:
+            if p.x < maxX:
+                if p.y > minY:
+                    if p.y < maxY:
+                        return True
+                    
+        return False
+    
+
+
+
+
